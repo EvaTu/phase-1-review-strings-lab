ba518ab5f45c6f490cddf34de95c0f5b922202cc
EvaTu <evatuw@gmail.com>
2021-08-21 07:56:07 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "Eva Tu";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const firstInitial = currentUser.slice(0,1);
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
